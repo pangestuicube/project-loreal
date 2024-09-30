51c490497684abfcfcca95629304c9f2c9685999
Pangestu <pangestu.kurniawan@sirclo.com>
2024-09-30 11:45:48 +0700

    add TC001.Successfully Login with Valid Credentials

--- resources/data/testdata.py
+++ resources/data/testdata.py
@@ -1,2 +1,9 @@
+# Timeout
+timeOutShort = 10
+timeOutMedium = 30
+timeOutLong = 60
+
 #URL List
-URLWEB                  = 'https://loreal-chat.testingnow.me/'
+URLWEB  = 'https://loreal-chat.testingnow.me/'
+LoginUsername    = 'mage2user'
+LoginPassword    = 'password123'
